cc18441b300decf3aedfc39c9eac7dd0b03043c7
hachiko-8ko <hachiko-8ko>
2021-05-17 20:39:25 -0400

    Remember to minify ... add it to the build script

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const ts = require('gulp-typescript');
 const tsProject = ts.createProject("tsconfig.json");
 const source = require('vinyl-source-stream');
 const browserify = require('browserify');
+const exec = require('child_process').exec
 
 gulp.task('build-ts', function () {
     return gulp.src('**/*.ts')
@@ -19,6 +20,16 @@ gulp.task('tests', ['build-ts'], function () {
     }).bundle()
         .pipe(source("ichigo-tests.js"))
         .pipe(gulp.dest('./tests_out'));
+});
+
+gulp.task('minify', function () {
+    exec('./minify.sh', function (err, stdout, stderr) {
+        console.log(stdout);
+        console.error(stderr);
+        if (err) {
+            console.error(err);
+        }
+    });
 });
 
 gulp.task('mi5-component', ['build-ts'], function () {
@@ -118,7 +129,9 @@ gulp.task('mi5-build-all', [
     'mi5-router',
     'mi5-util',
     'mi5-full'
-]);
+], function () {
+    gulp.start('minify');
+});
 
 // I set this up in keybindings.json (global). VSCode wouldn't let me set it up as a workspace setting.
 gulp.task('gulp-default', ['tests']);
--- minify.sh
+++ minify.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-uglifyjs  --compress --mangle --output dist/mi5-component.min.js dist/mi5-component.js
+uglifyjs  --compress --mangle 'reserved=[Component,BoundComponent]' --output dist/mi5-component.min.js dist/mi5-component.js
 uglifyjs  --compress --mangle --output dist/mi5-extension.min.js dist/mi5-extension.js
 uglifyjs  --compress --mangle --output dist/mi5-keyword.min.js dist/mi5-keyword.js
 uglifyjs  --compress --mangle --output dist/mi5-observable.min.js dist/mi5-observable.js
 uglifyjs  --compress --mangle --output dist/mi5-promise.min.js dist/mi5-promise.js
 uglifyjs  --compress --mangle --output dist/mi5-router.min.js dist/mi5-router.js
 uglifyjs  --compress --mangle --output dist/mi5-util.min.js dist/mi5-util.js
-uglifyjs  --compress --mangle --output dist/ichigo-full.min.js dist/ichigo-full.js
+uglifyjs  --compress --mangle 'reserved=[Component,BoundComponent]' --output dist/ichigo-full.min.js dist/ichigo-full.js
