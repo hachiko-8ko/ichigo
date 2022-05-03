6e3f83ed19ec4127f569110b96768c3689d385f7
hachiko-8ko <hachiko-8ko>
2022-05-03 11:06:50 -0400

    Add result declaration for the (very weird) delay function

--- src/System/Async/Delay.ts
+++ src/System/Async/Delay.ts
@@ -1,3 +1,3 @@
-export function delay(time: number) {
+export function delay(time: number): (result: any) => PromiseLike<any> {
     return (result: any) => new Promise(resolve => setTimeout(() => resolve(result), time));
 }
